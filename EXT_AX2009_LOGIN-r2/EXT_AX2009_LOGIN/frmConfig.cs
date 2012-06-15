717e3d9a00ba4b55bd2ac1a7c4a59d3df2b77469
AX2009 login <samano.srima@gmail.com>
2012-06-15 08:48:52 +0700

    loginr2
    loginr2

---
+++ EXT_AX2009_LOGIN-r2/EXT_AX2009_LOGIN/frmConfig.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Collections.Specialized;
+using System.Net;
+
+namespace EXT_AX2009_LOGIN
+{
+    public partial class frmConfig : Form
+    {
+        public frmConfig()
+        {
+            InitializeComponent();
+        }
+        Configuration config;
+        string hname;
+        string[] hostname;
+        int num;
+        private void frmConfig_Load(object sender, EventArgs e)
+        {
+            //IPHostEntry iphost = Dns.GetHostByName(Dns.GetHostName());
+
+            //Hostname
+            //MessageBox.Show("Hostname : " + iphost.HostName + "\r\n");
+            //hname = iphost.HostName;
+           // hostname = hname.Split('.');
+            
+            //foreach (string word in hostname)
+           // {
+              //  num++;
+               
+              //  if (hostname.Length == num)
+              //  {
+               //     MessageBox.Show(word.ToString());
+               // }
+           // }
+            //MessageBox.Show(hostname[2].ToString());
+           // config=ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+           // txtDomain.Text = config.AppSettings.Settings["domain"].Value;
+           // txtIP.Text = config.AppSettings.Settings["axserverip"].Value;
+           // txtPort.Text = config.AppSettings.Settings["axserverport"].Value;
+            txtDomain.Text = SettingNew.Default.domain.ToString();
+            txtIP.Text = SettingNew.Default.ip.ToString();
+            txtPort.Text = SettingNew.Default.port.ToString();
+            
+
+
+        }
+
+        private void cmdSave_Click(object sender, EventArgs e)
+        {
+            save();
+        }
+        private void save()
+        {
+            try
+            {
+
+               // config.AppSettings.Settings["domain"].Value = txtDomain.Text;
+               // config.AppSettings.Settings["axserverip"].Value = txtIP.Text;
+                //config.AppSettings.Settings["axserverport"].Value = txtPort.Text;
+                // Save the configuration file.
+                //config.Save(ConfigurationSaveMode.Modified);
+                // Force a reload of a changed section.
+                //ConfigurationManager.RefreshSection("appSettings");
+                //MessageBox.Show(config.AppSettings.Settings["domain"].Value);
+                SettingNew.Default.ip = txtIP.Text;
+                SettingNew.Default.port = txtPort.Text;
+                SettingNew.Default.domain = txtDomain.Text;
+                SettingNew.Default.Save();
+                this.Close();
+                MessageBox.Show("Change completed");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+           
+        }
+        private void txtDomain_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                txtIP.Focus();
+            }
+        }
+
+        private void txtIP_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                txtPort.Focus();
+            }
+        }
+
+        private void txtPort_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                save();
+            }
+        }
+    }
+}
---
+++ EXT_AX2009_LOGIN-r2/EXT_AX2009_LOGIN/frmLogin.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace EXT_AX2009_LOGIN
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPwd = new System.Windows.Forms.TextBox();
+            this.cmdLogin = new System.Windows.Forms.Button();
+            this.cmdCancel = new System.Windows.Forms.Button();
+            this.lblPing = new System.Windows.Forms.Label();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tsToolbar = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(217, 361);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 14);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(217, 391);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 14);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtUser.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.txtUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.txtUser.Location = new System.Drawing.Point(289, 360);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(110, 23);
+            this.txtUser.TabIndex = 2;
+            this.txtUser.TextChanged += new System.EventHandler(this.txtUser_TextChanged);
+            this.txtUser.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtUser_KeyDown);
+            // 
+            // txtPwd
+            // 
+            this.txtPwd.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtPwd.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.txtPwd.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.txtPwd.Location = new System.Drawing.Point(289, 389);
+            this.txtPwd.Name = "txtPwd";
+            this.txtPwd.PasswordChar = '*';
+            this.txtPwd.Size = new System.Drawing.Size(110, 23);
+            this.txtPwd.TabIndex = 3;
+            this.txtPwd.UseSystemPasswordChar = true;
+            this.txtPwd.TextChanged += new System.EventHandler(this.txtPwd_TextChanged);
+            this.txtPwd.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtPwd_KeyDown);
+            // 
+            // cmdLogin
+            // 
+            this.cmdLogin.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.cmdLogin.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.cmdLogin.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.cmdLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.cmdLogin.ForeColor = System.Drawing.Color.Black;
+            this.cmdLogin.Location = new System.Drawing.Point(283, 439);
+            this.cmdLogin.Name = "cmdLogin";
+            this.cmdLogin.Size = new System.Drawing.Size(55, 23);
+            this.cmdLogin.TabIndex = 4;
+            this.cmdLogin.Text = "Login";
+            this.cmdLogin.UseVisualStyleBackColor = false;
+            this.cmdLogin.Click += new System.EventHandler(this.cmdLogin_Click);
+            // 
+            // cmdCancel
+            // 
+            this.cmdCancel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.cmdCancel.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.cmdCancel.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.cmdCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.cmdCancel.ForeColor = System.Drawing.Color.Black;
+            this.cmdCancel.Location = new System.Drawing.Point(344, 439);
+            this.cmdCancel.Name = "cmdCancel";
+            this.cmdCancel.Size = new System.Drawing.Size(55, 23);
+            this.cmdCancel.TabIndex = 5;
+            this.cmdCancel.Text = "Cancel";
+            this.cmdCancel.UseVisualStyleBackColor = false;
+            this.cmdCancel.Click += new System.EventHandler(this.cmdCancel_Click);
+            // 
+            // lblPing
+            // 
+            this.lblPing.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblPing.AutoSize = true;
+            this.lblPing.BackColor = System.Drawing.Color.Gold;
+            this.lblPing.Location = new System.Drawing.Point(220, 268);
+            this.lblPing.Name = "lblPing";
+            this.lblPing.Size = new System.Drawing.Size(0, 13);
+            this.lblPing.TabIndex = 6;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsToolbar});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(658, 25);
+            this.toolStrip1.TabIndex = 7;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // tsToolbar
+            // 
+            this.tsToolbar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsToolbar.Image = ((System.Drawing.Image)(resources.GetObject("tsToolbar.Image")));
+            this.tsToolbar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsToolbar.Name = "tsToolbar";
+            this.tsToolbar.Size = new System.Drawing.Size(36, 22);
+            this.tsToolbar.Text = "Help";
+            this.tsToolbar.Click += new System.EventHandler(this.tsToolbar_Click);
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::EXT_AX2009_LOGIN.photo.LOGO_AX2009;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(658, 507);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.lblPing);
+            this.Controls.Add(this.cmdCancel);
+            this.Controls.Add(this.cmdLogin);
+            this.Controls.Add(this.txtPwd);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmLogin";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.frmLogin_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TextBox txtPwd;
+        private System.Windows.Forms.Button cmdLogin;
+        private System.Windows.Forms.Button cmdCancel;
+        private System.Windows.Forms.Label lblPing;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton tsToolbar;
+    }
+}
+
