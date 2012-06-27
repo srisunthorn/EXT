2c5dadaf49ee7f9e85deaf80f414b6c12e32c208
angpao <samano.srima@gmail.com>
2012-06-27 09:10:43 +0700

    r2
    rr2

---
+++ EXT_AX2009_LOGIN-R2/EXT_AX2009_LOGIN/DBConn.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EXT_AX2009_LOGIN
+{
+    static class DBConn
+    {
+        //static public string strConn = "Data Source=192.168.1.251;Initial Catalog=EXEDY_AX2009_LIVE_NEW;User Id=sa;Password=p@ssw0rd;";
+        static public string strConn = "Data Source=192.168.1.251;Initial Catalog=EXEDY_AX2009_LIVE_NEW;User Id=sa;Password=p@ssw0rd;";
+    }
+}
